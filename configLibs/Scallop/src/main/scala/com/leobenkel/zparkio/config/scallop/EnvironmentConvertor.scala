0267cf44ac2ced0486b648272ddcb5cc643c5395
Leo Benkel <leo.benkel@gmail.com>
2022-05-16 10:19:41 +0200

    fix

--- configLibs/Scallop/src/main/scala/com/leobenkel/zparkio/config/scallop/EnvironmentConvertor.scala
+++ configLibs/Scallop/src/main/scala/com/leobenkel/zparkio/config/scallop/EnvironmentConvertor.scala
@@ -12,16 +12,24 @@ object EnvironmentConverter extends DefaultConverters {
 
   val Parser: ValueConverter[EnvironmentConverter.ArgType] =
     new ValueConverter[EnvironmentConverter.ArgType] {
+      private object Extract {
+        def unapply(input: (String, List[String])): Option[String] =
+          input match {
+            case (_, i :: Nil) => Some(i)
+            case _             => None
+          }
+      }
+
       def parse(
           s: List[(String, List[String])]
       ): Either[String, Option[EnvironmentConverter.ArgType]] =
         s match {
-          case _, i :: Nil :: Nil => Environment.parseEnv(i) match {
-              case Some(e) => Right(Some(e))
-              case None    => Left(s"Cannot find valid environment for input: '$i'")
+          case Extract(i) :: Nil => Environment.parseEnv(i) match {
+              case e @ Some(_) => Right(e)
+              case None        => Left(s"Cannot find valid environment for input: '$i'")
             }
-          case Nil                => Right(None)
-          case _                  => Left("you should provide exactly one argument for this option")
+          case Nil               => Right(None)
+          case _                 => Left("you should provide exactly one argument for this option")
         }
 
       val argType: ArgType.V = ArgType.SINGLE
